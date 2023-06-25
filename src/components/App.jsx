2cfc7b82540bbf070891f1d969a9c22c132c0e60
Andrii Minyailo <andrii.minyailo@gmail.com>
2023-06-25 23:31:04 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,11 @@
+// import { TrendingToday } from './TrendingToday';
+// import { RequestServer } from 'requestServer';
+
+// const requestServer = new RequestServer();
+
+import { Home } from 'pages/Home';
+import { Routes, Route, NavLink, Link } from 'react-router-dom';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +15,21 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      {/* React homework template */}
+      {/* <TrendingToday /> */}
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/products">Products</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} /> */}
+      </Routes>
     </div>
   );
 };
