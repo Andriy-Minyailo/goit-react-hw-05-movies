693deb66fc94e3021406108579d7ed5fee5859da
Андрій <109473689+Andriy-Minyailo@users.noreply.github.com>
2023-06-30 00:29:16 +0300

    useSearchParams

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,5 +1,6 @@
 import { ListMovies } from 'components/ListMovies/ListMovies';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { RequestServer } from 'requestServer';
 
 const requestServer = new RequestServer();
@@ -7,25 +8,45 @@ const requestServer = new RequestServer();
 export const Movies = () => {
   const [inputValue, setInputValue] = useState('');
   const [arraySearchMovies, setArraySearchMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query');
 
-  const submitSearchMovies = async () => {
-    try {
-      const {
-        data: { results },
-      } = await requestServer.searchMovies(inputValue);
-      setArraySearchMovies(results);
-      setInputValue('');
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const submitSearchMovies = async () => {
+  //   try {
+  //     const {
+  //       data: { results },
+  //     } = await requestServer.searchMovies(inputValue);
+  //     setArraySearchMovies(results);
+  //     setSearchParams({ query: inputValue });
+  //     setInputValue('');
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  useEffect(() => {
+    const submitSearchMovies = async () => {
+      try {
+        const {
+          data: { results },
+        } = await requestServer.searchMovies(query);
+        setArraySearchMovies(results);
+        // setInputValue('');
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    submitSearchMovies();
+  }, [query]);
+
   return (
     <>
       <input
         onChange={({ target: { value } }) => setInputValue(value)}
         value={inputValue}
       ></input>
-      <button onClick={submitSearchMovies}>Search</button>
+      <button onClick={setSearchParams({ query: inputValue })}>Search</button>
       <ListMovies arrayMovies={arraySearchMovies} />
     </>
   );
